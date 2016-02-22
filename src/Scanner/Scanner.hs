37a91c7bb0b94a7a6043c1855b84794ff0711975
Pedro Martins <pedro.martins08@imperial.ac.uk>
2016-02-22 15:14:20 +0000

    Fixed command line help

--- src/Scanner/Scanner.hs
+++ src/Scanner/Scanner.hs
@@ -44,23 +44,23 @@ processArgs r = subparser
   (  (rcommand "register" registerNode parseNodeCapRecord)
   <> (rcommand "unregister" unregisterNode ((Just <$> argument str idm) <|> pure Nothing))
   <> (rcommand "query" (queryNodesDB . read . capitalize) (argument str idm))
-  <> (command "deploy" (info (fmap (\(RemoteArgs mr args) ->
+  <> (command "deploy" (info (helper <*> fmap (\(RemoteArgs mr args) ->
                                       case words args of
                                         (fname:opts) -> void $ deployApp mr Exec fname opts)
                              (parseArgs r (argument str idm))) idm))
-  <> (command "deployLocal" (info (fmap (\(RemoteArgs mr args) ->
+  <> (command "deployLocal" (info (helper <*> fmap (\(RemoteArgs mr args) ->
                                           case words args of
                                             (fname:opts) -> void $ deployApp mr RunLocal fname opts)
                                   (parseArgs r (argument str idm))) idm))
   <> (rcommand "display" dumpNodes (pure ()))
-  <> (command "readconfig" (info (pure (readConfig >>= print)) idm))
-  <> (command "init" (info (pure initDatabase) idm)))
+  <> (command "readconfig" (info (helper <*> pure (readConfig >>= print)) idm))
+  <> (command "init" (info (helper <*> pure initDatabase) idm)))
   where
     capitalize [] = []
     capitalize (c:cs) = (toUpper c:map toLower cs)
 
     rcommand :: forall a b. (XmlRpcType a, XmlRpcType b, Show b) => String -> (a -> IO b) -> Parser a -> Mod CommandFields (IO ())
-    rcommand c f p = command c (info (fmap handle (parseArgs r p)) idm)
+    rcommand c f p = command c (info (helper <*> fmap handle (parseArgs r p)) idm)
       where handle (RemoteArgs Nothing args) = (f >=> print) args
             handle (RemoteArgs (Just r) args) = ((remote r c :: a -> IO b) >=> print) args
 
@@ -68,5 +68,5 @@ main :: IO ()
 main = do
   (_, oh, _, _) <- runInteractiveCommand "curl http://www.doc.ic.ac.uk/~pm1108/scaffold/dynIP"
   r <- hGetContents oh
-  join $ execParser (info (processArgs r) idm)
+  join $ execParser (info (helper <*> processArgs r) idm)
 
